b817dfbe80c70643515fd22b335524be95f5475a
Rhea Parekh <rheaparekh12@gmail.com>
2018-02-08 16:10:38 +0530

    Correct paths

--- data/get_coordinates.py
+++ data/get_coordinates.py
@@ -6,8 +6,21 @@ the incoming object.
 
 import os
 
-def get_coordinates(file):
-  with file  as file:
+def objects_path():
+    curr_coordinate = {} # dict to hold the center of the objects detected in the present image
+    prev_coordinate = {} # dict to hold the center of the objects detected in the previous image
+    yolo_output = open('./yolo_output.txt', 'r')
+
+    curr_coordinate = get_coordinates(yolo_output)
+    file2 = open('./yolo_output_prev.txt','w+')
+    if os.stat('./yolo_output_prev.txt').st_size == 0:
+		os.system('cat ./yolo_output.txt>./yolo_output_prev.txt')
+    prev_coordinate = get_coordinates(file2)
+    os.system('cat ./yolo_output.txt>yolo_output_prev.txt')
+    return [curr_coordinate, prev_coordinate]
+
+def get_coordinates(yolo_output):
+  with yolo_output as file:
     lines = file.readlines()
   coordinate_map = {}
   for i in range(len(lines)):
@@ -21,17 +34,4 @@ def get_coordinates(file):
 
       # Returns center X and Y co-ordinate
       coordinate_map[item]  = [(left+right)/2, (top+bottom)/2]
-  return coordinate_map
-
-def objects_path():
-	curr_coordinate = {} # dict to hold the center of the objects detected in the present image
-	prev_coordinate = {} # dict to hold the center of the objects detected in the previous image
-	file1 =  open('./yolo_output.txt','r')
-	curr_coordinate = get_coordinates(file1)
-	file2 = open('./yolo_output_prev.txt','w+')
-	if os.stat('./yolo_output_prev.txt').st_size == 0:
-		os.system('cat ./yolo_output.txt>./yolo_output_prev.txt')
-	prev_coordinate = get_coordinates(file2)
-	os.system('cat ./yolo_output.txt>yolo_output_prev.txt')
-	return [curr_coordinate, prev_coordinate]
-
+      return coordinate_map
--- data/saveTheClash.py
+++ data/saveTheClash.py
@@ -3,6 +3,7 @@ from get_coordinates import * # use to get center coordinates of detected image
 import os
 import subprocess
 import numpy
+import logging
 #import matplotlib.pyplot as plt
 from PIL import Image 
 
@@ -11,55 +12,53 @@ file contains the function for the search and view mode.
 """
 
 def saveTheClash():
-	
 	#load the keyword
-	keyword = open('./Data/output_local.txt','r').read() # decides the function to be executed
+	keyword = open('./Input/send/output_local.txt', 'r').read() # decides the function to be executed
 	print(keyword)
 	if 'search' in keyword:
 		# (bash script to run the YOLO >> objects_detected.txt)
                 os.chdir('../defaults/darknet')
 		# process the file here and generate the string
-                os.system("./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights ~/Templates/data/Input/shot.jpg >~/Templates/data/yolo_output.txt")
+                os.system("./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights ~/Templates/we-dont-know-da-wae/data/Input/send/shot.jpg >../../data/yolo_output.txt")
                 os.chdir('../../data/')
 		output = "is in front of you! "
 		yolo_output = open('./yolo_output.txt','r').read()
+                print(yolo_output)
 		keywords = set(keyword.split())
-                print(keywords)
 		for i in keywords:
-			if i in yolo_output:
-				output = i + " " + output
-                		print(output)
-				file1 = open("./output.txt", "w")
-				file1.write(output) 		
-		# os.system('rm ./Input/image_test.jpg')
+	          if i in yolo_output:
+			output = i + " " + output
+			file1 = open("./output.txt", "w")
+			file1.write(output) 
+		os.system('rm -r ./Input/send')
 		# use scp to send a txt file to the intermediate and convert it to speech
 
 	elif 'view' in keyword:
 		# (bash script to run stereoCNN)
-		output = ""
 		detected_objects = []
-		hue = [[243, 234, 49], [112, 55, 168]] # will be an array of pixels
+		hue = [[255, 234, 49], [112, 55, 168]] # will be an array of pixels
 		os.chdir('../defaults/darknet')
-		subprocess.Popen('./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights ~/Templates/data/Input/shot.jpg >./yolo_output.txt', shell=True)
-		print("it works")
-		os.chdir('../monodepth')
-		subprocess.call('python monodepth_simple.py --image_path ~/Templates/data/Input/image_test_far.jpg --checkpoint_path ~/Templates/defaults/monodepth/models/model_cityscapes',shell=True)
+
+                os.system("./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights ~/Templates/we-dont-know-da-wae/data/Input/send/shot.jpg >../../data/yolo_output.txt")
+
 		os.chdir('../../data')
-		print("nets ran")
-		stereo_image = Image.open('./Input/shot_disp.png')
+		os.chdir('../defaults/monodepth')
+
+		subprocess.call('python monodepth_simple.py --image_path ~/Templates/we-dont-know-da-wae/data/Input/send/shot.jpg --checkpoint_path ~/Templates/we-dont-know-da-wae/defaults/monodepth/models/model_cityscapes',shell=True)
+		os.chdir('../../data')
+
+		stereo_image = Image.open('./Input/send/shot_disp.png')
 		stereo_image = stereo_image.load()
-		coordinates = objects_path()
-		curr_coordinates = coordinates[0]
-		prev_coordinates = coordinates[1]
-		print(curr_coordinates)
-		print(prev_coordinates)		
+		curr_coordinates, prev_coordinates = objects_path()
+
+		output = ""
 		for key in curr_coordinates.keys():
 			print(curr_coordinates[key][0])
                         stereo_coordinates = stereo_image[curr_coordinates[key][0], curr_coordinates[key][1]][:3]
 			print(stereo_coordinates)
 			if stereo_coordinates[0] <= hue[0][0] and stereo_coordinates[0] >= hue[1][0] and  \
-                           stereo_coordinates[1] >= hue[0][1] and stereo_coordinates[1] <= hue[1][1] and  \
-                           stereo_coordinates[2] <= hue[0][2] and stereo_coordinates[2] >= hue[1][2]:     
+                           stereo_coordinates[1] >= hue[1][1] and stereo_coordinates[1] <= hue[0][1] and  \
+                           stereo_coordinates[2] <= hue[1][2] and stereo_coordinates[2] >= hue[0][2]:     
 				if key in prev_coordinates.keys():
 					if(curr_coordinates[key][0]-prev_coordinates[key][0] > 0):
 						output = output + " " + key + " approaching you from left"
@@ -70,7 +69,9 @@ def saveTheClash():
                                 else:
                                         output = output + " " + key + " is approaching you."
 		file1 = open("./output.txt","w")
+                print(output)
 		file1.write(output)
+ 		os.system('rm -r ./Input/send')
 
 	elif keyword == 'view_disabled' :
 		pass 
